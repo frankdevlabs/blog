8696886c43abfa65698c04b76bcdc0c3f31893c3
Frank de Vries <f.devries@outlook.com>
2023-11-30 21:45:10 +0100

    updated the readme files

--- web/README.md
+++ web/README.md
@@ -1,1 +1,14 @@
-# blog-web
+# Web
+
+This folder contains the frontend code for Frank's Blog. It's built using Gatsby and integrates with Sanity.io for content management.
+
+## Setup
+
+To set up the frontend:
+
+1. Run `npm install` to install dependencies.
+2. Use `npm run develop` to start the local development server.
+
+## Deployment
+
+The frontend is configured for automatic deployment via Netlify when changes are pushed to the master branch.
--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -22,7 +22,7 @@ const FILE_NAME = process.env.GATSBY_TAG_CONTAINER_FILE_NAME
 
 const gtmLoadScript = `
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'${FILE_NAME}'});var f=d.getElementsByTagName(s)[0],
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       '${process.env.GATSBY_TAG_CONTAINER_URL}/${FILE_NAME}?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','${process.env.GATSBY_GTM_ID}');
